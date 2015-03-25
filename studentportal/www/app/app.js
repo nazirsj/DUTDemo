400cf4b3c4e05ebae81d9aa6283015cd90762f6e
Nazir Jooman <nazirj@sahomeloans.com>
2015-03-25 17:16:50 +0200

    folder structure

---
+++ studentportal/www/app/app.js
@@ -0,0 +1,42 @@
+angular.module("studentportalApp",["ionic"])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+.config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+	.state('app', {
+      abstract: true,
+      url: "/app",
+      templateUrl: "app/menu/sidemenu.html"
+    })
+	.state('app.home', {
+      url: "/home",
+      views: {
+      		'mainContent': {
+      			templateUrl: "app/home/home.html"
+      		}
+  		}
+    })
+    .state('app.students', {
+      url: "/students",
+      views: {
+          'mainContent': {
+            templateUrl: "app/students/students.html"
+          }
+      }
+    });
+
+    // if none of the above states are matched, use this as the fallback
+  	$urlRouterProvider.otherwise('/app/home');
+});
